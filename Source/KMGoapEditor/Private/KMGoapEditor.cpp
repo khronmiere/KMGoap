efe31c42d320fb7e91a2f3d80e5fc221fad6992e
Rafael Ocariz <rafael.ocariz90@gmail.com>
2025-12-12 10:57:44 -0500

    Added editor module to the plugin

---
+++ Source/KMGoapEditor/Private/KMGoapEditor.cpp
@@ -0,0 +1,17 @@
+﻿#include "KMGoapEditor.h"
+
+#define LOCTEXT_NAMESPACE "FKMGoapEditorModule"
+
+void FKMGoapEditorModule::StartupModule()
+{
+    
+}
+
+void FKMGoapEditorModule::ShutdownModule()
+{
+    
+}
+
+#undef LOCTEXT_NAMESPACE
+    
+IMPLEMENT_MODULE(FKMGoapEditorModule, KMGoapEditor)
